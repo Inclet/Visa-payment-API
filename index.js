a5fbec867095f8153084ae6d38946a3025cd20af
Clet Mwunguzi <inclet@Clets-MacBook-Pro.local>
2019-05-12 12:14:23 +0200

    add cors module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import routes from './routes/payment'
+import cors from 'cors';
 
 
 dotenv.config();
@@ -12,6 +13,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors())
 
 app.use('/api', routes)
 
